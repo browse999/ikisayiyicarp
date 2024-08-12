94d96744e82174705ead4ee320844316d8ca413a
Browser <92232900+browse999@users.noreply.github.com>
2024-08-12 19:44:39 +0300

    Add files via upload

---
+++ sayilari_carp.js
@@ -0,0 +1,9 @@
+
+//  iki sayının karesini alan program
+
+sayi1 = Number(prompt('Birinci sayiyi giriniz'))
+
+sayi2 = Number(prompt('İkinci sayıyı giriniz'))
+
+
+kare = Number(alert(`İki Sayının Çarpım Sonucu: `+ sayi1*sayi2))
